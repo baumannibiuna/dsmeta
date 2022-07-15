c8223f44f8a3549d5b8abc26358f83481849a2c0
baumannibiuna <baumannibiuna@gmail.com>
2022-07-15 18:54:35 -0300

    First Component

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,11 @@
-function NotificationButton(){
-    return(
-        <p>Meu componente</p>
+import icon from '../../assets/img/notification-icon.svg'
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar" />
+        </div>
     )
 }
 
